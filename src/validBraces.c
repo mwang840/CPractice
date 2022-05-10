03358b3d73b18c2b90afdbac9f0e48903d3cbf9f
Maxwell Wang <maxwang@udel.edu>
2022-05-10 05:28:59 -0400

    working on validbraces for now and configured launch file

---
+++ src/validBraces.c
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+#include <stdio.h>
+bool valid_braces (const char *braces)
+{
+    char *avocadoBrace;
+    bool braceForImpact = true;
+    for(avocadoBrace = braces; avocadoBrace != '\0'; avocadoBrace++){
+        
+    }
+	return false;
+}
