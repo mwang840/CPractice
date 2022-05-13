20f4ff56d366254c244d6efcf95b47f379d5a844
Maxwell Wang <maxwang@udel.edu>
2022-05-13 09:52:11 -0400

    initialized other function headers

--- src/validBraces.h
+++ src/validBraces.h
@@ -1,5 +1,9 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <limits.h>
 
 int pop(struct braceNode *node);
 void push(struct braceNode *node, int information);
+struct braceNode createStack(unsigned fatness);
+int isFull(struct braceNode *node);
+int isEmpty(struct braceNode *node);
