94143c7e2130b074b1a0162776c853eb61c9a95d
Maxwell Wang <maxwang@udel.edu>
2022-07-30 21:38:32 -0400

    arthemic layout

---
+++ src/Arthemic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int findMissing(const int *nums, size_t n);
+
+int main(){
+    return 0;
+}
+
+int findMissing(const int *nums, size_t n){
+    int *end = nums[n-1];
+    int missing = 0;
+
+    for(int *start = nums; start!=end; start++){
+        
+    }
+
+}
