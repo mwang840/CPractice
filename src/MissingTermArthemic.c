f9886def233fcb0faa5d89e7fa82acbed33c8ad0
Maxwell Wang <maxwang@udel.edu>
2022-08-13 20:29:59 -0400

    missing arthemic

--- src/MissingTermArthemic.c
+++ src/MissingTermArthemic.c
@@ -5,15 +5,25 @@
 int findMissing(const int *nums, size_t n);
 
 int main(){
+    size_t size;
+    const int *nums;
+    scanf("Enter a fixed size: &u", &size);
+    for(size_t i = 0; i < size; ++i){
+        scanf("Enter a number &u", &i);
+    }
+    scanf("Enter an array of ints");
     return 0;
 }
 
 int findMissing(const int *nums, size_t n){
     int *end = nums[n-1];
+    int *begining = nums[0];
     int missing = 0;
-    int difference = 0;
-    for(int *start = nums; start!=end; start++){
-        
+    int *difference = (&end - &begining)/n;
+    for(unsigned int i = 0; i < n - 1; ++i){
+        if(&nums[i + 1] - &nums[i] !=difference){
+            return &nums[i] + *difference;
+        }
     }
 
 }
